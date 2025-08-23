6a22652c371846113593c19039395d61b13554a3
Andressa Raffler <andressa.raffler@gmail.com>
2025-08-23 20:57:58 -0300

    Initial commit

---
+++ src/main/java/com/raffler/envpermittingragjava/EnvPermittingRagJavaApplication.java
@@ -0,0 +1,13 @@
+package com.raffler.envpermittingragjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvPermittingRagJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvPermittingRagJavaApplication.class, args);
+	}
+
+}
