50a72516f603931a77225852be7dc86f5336a5ee
Andressa Raffler <andressa.raffler@gmail.com>
2025-08-24 15:14:18 -0300

    feat(milestone0): added integration test spring profile

--- src/test/java/com/raffler/env_permitting_rag_java/EnvPermittingRagJavaApplicationTests.java
+++ src/test/java/com/raffler/env_permitting_rag_java/EnvPermittingRagJavaApplicationTests.java
@@ -2,8 +2,10 @@ package com.raffler.env_permitting_rag_java;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EnvPermittingRagJavaApplicationTests {
 
     @Test
